7a40ac150f97779690422cd0c5cbb716a559b3d6
root <root@ip-172-31-89-234.ec2.internal>
2025-04-02 02:14:27 +0000

    First commit to test commit function #story 123

---
+++ sample1.java
@@ -0,0 +1,13 @@
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        // Define two numbers
+        int num1 = 10;
+        int num2 = 20;
+
+        // Add the numbers
+        int sum = num1 + num2;
+
+        // Print the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
