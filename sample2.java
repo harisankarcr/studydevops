500b6ede8faebd1d1d3960d75db52a1a3209d54d
root <root@ip-172-31-89-234.ec2.internal>
2025-04-04 10:29:43 +0000

    Addded sample2.jave with array code

---
+++ sample2.java
@@ -0,0 +1,11 @@
+public class ArrayExample {
+    public static void main(String[] args) {
+        // Define an array of integers
+        int[] numbers = {1, 2, 3, 4, 5};
+
+        // Use a for loop to iterate over the array
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Element at index " + i + ": " + numbers[i]);
+        }
+    }
+}
